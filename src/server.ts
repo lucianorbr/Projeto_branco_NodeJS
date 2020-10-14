3a9796a22cd8654fcc102ae81f9260cadc650855
Luciano Ribeiro <luciano.ribeiro@outlook.com>
2020-10-14 13:41:27 -0300

    Setup de projeto NodeJS, para utilizado pela Rocketseat atualemente em Outubro 2020

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import '@controllers/UsersController'
+
+const app = express()
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Gabriela' })
+})
+
+app.listen(3333)
